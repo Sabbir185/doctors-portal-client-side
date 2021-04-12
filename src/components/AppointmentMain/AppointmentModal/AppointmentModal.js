0a22c1398759ed62127910c085618d3c35855b06
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-04-12 16:19:55 +0600

    modal updated

--- src/components/AppointmentMain/AppointmentModal/AppointmentModal.js
+++ src/components/AppointmentMain/AppointmentModal/AppointmentModal.js
@@ -9,7 +9,8 @@ const customStyles = {
         right: 'auto',
         bottom: 'auto',
         marginRight: '-50%',
-        transform: 'translate(-50%, -50%)'
+        transform: 'translate(-50%, -50%)',
+        boxShadow:'5px 5px 30px gray'
     }
 };
 
@@ -30,11 +31,41 @@ const AppointmentModal = ({ modalIsOpen, closeModal, title }) => {
                 <h2 className="text-center brand-text1">{title}</h2>
                 <form onSubmit={handleSubmit(onSubmit)}>
 
-                    <input defaultValue="test" {...register("example")} />
-                    <input {...register("exampleRequired", { required: true })} />
-                    {errors.exampleRequired && <span>This field is required</span>}
+                    <input type='text' {...register("fullName", { required: true })} className="form-control mb-3" placeholder="Full Name"
+                        select
+                    />
+                    {errors.fullName && <span>Name is required</span>}
 
-                    <input type="submit" />
+                    <input type='number' {...register("phoneNumber", { required: true })} className="form-control mb-3" placeholder="Phone Number"/>
+                    {errors.phoneNumber && <span>Phone Number is required</span>}
+
+                    <input type='email' {...register("email", { required: false })} className="form-control mb-3" placeholder="Email Address if any"/>
+                    {errors.email && <span>Email is Optional</span>}
+
+                    <input type='date' {...register("date", { required: true })} className="mb-3 form-control"/>
+                    {errors.date && <span>Date is required</span>}
+
+                    <div className="row mb-3">
+                        <div className="col-4" >
+                            <select name="gender" className='form-control' id="" {...register("gender", { required: true })}>
+                                <option disabled={true} value="set gender">Select Gender</option>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                                <option value="not set">Other</option>
+                            </select>
+                            {errors.gender && <span>Date is required</span>}
+                        </div>
+                        <div className="col-4">
+                            <input type='number' {...register("age", { required: true })} className="form-control" placeholder="Age"/>
+                            {errors.age && <span>Age is required</span>}                            
+                        </div>
+                        <div className="col-4">
+                            <input type='number' {...register("weight", { required: false })} className="form-control" placeholder="weight"/>
+                            {errors.weight && <span>weight is Optional</span>} 
+                        </div>
+                    </div>
+
+                    <input className="btn btn-info d-block ms-auto" type="submit" />
                 </form>
             </Modal>
         </div>
